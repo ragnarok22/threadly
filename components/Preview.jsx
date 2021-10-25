77f74ea643ea32fc096fd466b627ff7d1aa35575
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-25 14:15:31 -0400

    feat: agregando visualidad del preview

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,7 +1,10 @@
+import { Thread } from "./twitter/Thread"
+
 export const Preview = ({ className }) => {
   return (
-    <div className={`${className} bg-red-500`}>
-      <h1>Vista previa</h1>
+    <div className={`${className} overflow-auto`} style={{maxHeight: '90vh'}}>
+      <h1 className="text-center text-2xl">Vista previa</h1>
+      <Thread /> 
     </div>
   )
 }
