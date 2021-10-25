37be7d3fffb79e7fe4618534b79d3ba4ce61aa48
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-25 12:39:53 -0400

    feat: mejorando el footer visualmente

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -1,3 +1,11 @@
 export const Footer = () => (
-  <footer>Create by Ragnarok22</footer>
+  <footer className="px-5 py-3 bg-gray-800 text-gray-300 mt-auto flex items-center justify-between w-full">
+    <div>
+      &copy; Creado por <a href="https://twitter.com/LimbatusDev">Limbatus</a>. Todos los derechos reservados
+    </div>
+    <div className="flex justify-around">
+      <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Política de privacidad</a>
+      <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Términos y condiciones</a>
+    </div>
+  </footer>
 )
--- pages/components/navbar/index.jsx
+++ pages/components/navbar/index.jsx
@@ -11,14 +11,11 @@ const user = {
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
 }
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Compone', href: '#', current: true },
+  { name: 'En cola', href: '#', current: false },
+  { name: 'Estadísticas (pronto)', href: '#', current: false },
 ]
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
 ]
