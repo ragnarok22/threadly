268d0591f551fea53487605505f417aec8ef324c
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-28 14:28:48 -0400

    fix: error visual del preview con el user menu

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -85,7 +85,7 @@ export const Sidebar = () => {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
                         {userNavigation.map((item) => (
                           <Menu.Item key={item.name}>
                             {({ active }) => (
