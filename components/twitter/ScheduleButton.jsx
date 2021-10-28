f284f0dce29f8a2b2f0921b5eba2ffb65364450e
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-28 14:54:15 -0400

    fix: desabilitado el boton agendar

--- components/twitter/ScheduleButton.jsx
+++ components/twitter/ScheduleButton.jsx
@@ -4,10 +4,11 @@ export const ScheduleButton = ({ tweets, canTweet }) => {
   return (
     <button
       className="mx-3 flex justify-center items-center bg-yellow-500 text-gray-100 p-2 rounded-full w-full disabled:opacity-50 disabled:cursor-not-allowed"
-      disabled={!canTweet}
+      // disabled={!canTweet}
+      disabled={true}
     >
       <CalendarIcon className="h-6 w-6 mr-2" />
-      Agendar
+      Agendar (pronto)
     </button>
   )
 }
