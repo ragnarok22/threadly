e331a05fbceeb8cab39cdd40f3e666aa3c655357
Reinier Hernández <sasuke.reinier@gmail.com>
2021-10-31 08:53:21 -0400

    fix: arreglado modal en modo oscuro

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -6,18 +6,18 @@ export const Modal = ({ open, setOpen, title, children, icon, cancelable, closea
   const cancelButtonRef = useRef(null)
 
   const getIcon = () => {
-    let iconType = <QuestionMarkCircleIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+    let iconType = <QuestionMarkCircleIcon className="h-6 w-6 text-gray-600 dark:text-gray-200" aria-hidden="true" />
     let bg_class = 'mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10'
-    let class_type = 'bg-gray-100'
+    let class_type = 'bg-gray-100 dark:bg-gray-900'
     if (icon === 'warning') {
-      iconType = <ExclamationIcon className="h-6 w-6 text-yellow-600" aria-hidden="true" />
-      class_type = 'bg-yellow-100'
+      iconType = <ExclamationIcon className="h-6 w-6 text-yellow-600 dark:text-yellow-200" aria-hidden="true" />
+      class_type = 'bg-yellow-100 dark:bg-yellow-900'
     } else if (icon === 'info') {
-      iconType = <InformationCircleIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-      class_type = 'bg-green-100'
+      iconType = <InformationCircleIcon className="h-6 w-6 text-green-600 dark:text-green-200" aria-hidden="true" />
+      class_type = 'bg-green-100 dark:bg-green-900'
     } else if (icon === 'error') {
-      iconType = <ExclamationCircleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
-      class_type = 'bg-red-100'
+      iconType = <ExclamationCircleIcon className="h-6 w-6 text-red-600 dark:text-red-200" aria-hidden="true" />
+      class_type = 'bg-red-100 dark:bg-red-900'
     }
     return (
       <div className={`${bg_class} ${class_type}`}>
@@ -61,16 +61,16 @@ export const Modal = ({ open, setOpen, title, children, icon, cancelable, closea
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="inline-block align-bottom bg-white dark:bg-gray-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white dark:bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                   {getIcon()}
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
                       {title}
                     </Dialog.Title>
                     <div className="mt-2">
-                      <div className="text-sm text-gray-500">
+                      <div className="text-sm text-gray-500 dark:text-gray-300">
                         { children }
                       </div>
                     </div>
@@ -79,14 +79,22 @@ export const Modal = ({ open, setOpen, title, children, icon, cancelable, closea
               </div>
               {
                 cancelable &&
-                <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div className="bg-gray-50 dark:bg-gray-800 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => setOpen(false)}
+                  ref={cancelButtonRef}
+                >
+                  Cancelar
+                </button>
                   <button
                     type="button"
                     className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
-                    Cancel
+                    Cancelar
                   </button>
                 </div>
               }
