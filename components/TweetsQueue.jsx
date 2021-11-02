e4e1996223e935a0a17799575b48b859b243bfaa
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-01 22:37:19 -0400

    fix: mejorando el responsive del listado de tweets agendados

--- components/TweetsQueue.jsx
+++ components/TweetsQueue.jsx
@@ -1,8 +1,14 @@
+import { XCircleIcon, XIcon } from "@heroicons/react/outline"
 import moment from "moment"
 
 const TweetsQueue = ({ title, queues, showDate }) => {
+  const handleDeleteQueue = (e, id) => {
+    e.preventDefault()
+    console.log("delete queue", id)
+  }
+
   return (
-    <div className="w-1/3">
+    <div className="w-full px-5 lg:w-1/2">
       <h1 className="text-center text-2xl mt-5 mb-2">{ title }</h1>
       { 
         queues &&
@@ -16,11 +22,18 @@ const TweetsQueue = ({ title, queues, showDate }) => {
                   ))
                 }
               </div>
-              {
-                showDate
-                ? <p className="text-sm text-gray-400">{moment(queue.pubDate).format('DD/MM/YYYY HH:mm')}</p>
-                : <p className="text-sm text-gray-400">{moment(queue.pubDate).format('HH:mm')}</p>
-              }
+              <div className="flex items-center">
+                {
+                  showDate
+                  ? <p className="text-sm text-gray-400">{moment(queue.pubDate).format('DD/MM/YYYY HH:mm')}</p>
+                  : <p className="text-sm text-gray-400">{moment(queue.pubDate).format('HH:mm')}</p>
+                }
+                <XCircleIcon
+                  className="h-4 w-4 ml-3 text-red-600"
+                  aria-hidden="true"
+                  onClick={(e) => handleDeleteQueue(e, queue.id)}
+                />
+              </div>
             </div>
           ))
         : <p className="text-sm text-center text-gray-500 dark:text-gray-600">No hay tweets</p>
