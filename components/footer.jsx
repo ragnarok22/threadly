4288f1cdcb98bf5f6ea5ee38b571d361aa735203
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-02 15:15:14 -0400

    feat: agregado pagina de privacidad y tac

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,11 +1,22 @@
+import Link from 'next/link'
+
 export const Footer = () => (
   <footer className="px-5 py-3 bg-gray-800 text-gray-300 mt-auto flex flex-col md:flex-row items-center justify-between text-center w-full">
     <div className="mb-4 md:mb-0">
       &copy; Creado por <a href="https://twitter.com/LimbatusDev">Limbatus</a>. Todos los derechos reservados
     </div>
     <div className="flex justify-around">
-      <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Política de privacidad</a>
-      <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Términos y condiciones</a>
+      <Link href="/privacy">
+        <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+        Política de privacidad
+        </a>
+      </Link>
+      <Link href="/tac">
+        <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+        Términos y condiciones
+        </a>
+      </Link>
+        
     </div>
   </footer>
 )
---
+++ pages/privacy.jsx
@@ -0,0 +1,109 @@
+export default function Privacy() {
+  return (
+    <div className="privacy">
+      <h1>Privacy Policy</h1>
+      <p>
+        This privacy policy has been compiled to better serve those who are
+        concerned with how their &quot;Personally Identifiable Information&quot; (PII) is
+        being used online. PII, as described in US privacy law and information
+        security, is information that can be used on its own or with other
+        information to identify, contact, or locate a single person, or to
+        identify an individual in context. Please read our privacy policy
+        carefully to get a clear understanding of how we collect, use, protect
+        or otherwise handle your Personally Identifiable Information in
+        accordance with our website.
+      </p>
+      <h2>What personal information do we collect from the people that visit our
+        blog, website or app?
+      </h2>
+      <p>When ordering or registering on our site, as appropriate, you may be
+        asked to enter your name, email address, mailing address, phone number,
+        credit card information or other details to help you with your
+        experience.
+      </p>
+      <h2>When do we collect information?</h2>
+      <p>We collect information from you when you register on our site,
+        subscribe to a newsletter, fill out a form, enter a contest or
+        promotion, respond to a survey, or otherwise interact with us.
+      </p>
+      <h2>How do we use your information?</h2>
+      <p>We may use the information we collect from you when you register, make a
+        purchase, sign up for our newsletter, respond to a survey,
+        marketing or promotional communication, surf the website, or use certain
+        other site features in the following ways:
+      </p>
+      <ul>
+        <li>To personalize user&quot;s experience and to allow us to deliver the
+          type of content and product offerings in which you are most
+          interested.
+        </li>
+        <li>To improve our website in order to better serve you.
+        </li>
+        <li>To allow us to better service you in responding to your
+          customer service requests.
+        </li>
+        <li>To administer a contest, promotion, survey or other site feature.
+        </li>
+        <li>To quickly process your transactions.
+        </li>
+        <li>To send periodic emails regarding your order or other products and
+          services.
+        </li>
+      </ul>
+      <h2>How do we protect visitor information?</h2>
+      <p>Our website is scanned on a regular basis for security holes and known
+        vulnerabilities in order to make your visit to our site as safe as
+        possible.
+      </p>
+      <p>We use regular Malware Scanning.
+      </p>
+      <p>Your personal information is contained behind secured networks and is
+        only accessible by a limited number of persons who have special access
+        rights to such systems, and are required to keep the information
+        confidential.
+      </p>
+      <p>
+        In addition, all sensitive/credit information you supply is encrypted
+        via Secure Socket Layer (SSL) technology.
+      </p>
+      <p>We implement a variety of security measures when a user places an order
+        enters, submits, or accesses their information to maintain the safety
+        of your personal information.
+      </p>
+      <p>All transactions are processed through a gateway provider and are not
+        stored or processed on our servers.
+      </p>
+      <h2>Do we use &quot;cookies&quot;?</h2>
+      <p>Yes. Cookies are small files that a site or its service provider
+        transfers to your computer&quot;s hard drive through your Web browser (if you
+        allow) that enables the site&quot;s or service provider&quot;s systems to
+        recognize your browser and capture and remember certain information.
+      </p>
+      <p>We use cookies to understand and save your preferences for future
+        visits.
+      </p>
+      <p>You can choose to have your computer warn you each time a cookie is
+        being sent, or you can choose to turn off all cookies. You do this via
+        your browser (like Internet Explorer) settings. Each browser is a little
+        different, so look at your browser&quot;s Help menu to learn the correct
+        way to modify your cookies.
+      </p>
+      <p>If you disable cookies off, some features will be disabled that make
+        your site experience more efficient and some of our services will not
+        function properly.
+      </p>
+      <h2>Third-party disclosure</h2>
+      <p>We do not sell, trade, or otherwise transfer to outside parties your
+        Personally Identifiable Information.
+      </p>
+      <h2>Third-party links</h2>
+      <p>Occasionally, at our discretion, we may include or offer third-party
+        products or services on our website. These third-party sites have
+        separate and independent privacy policies. We therefore have no
+        responsibility or liability for the content and activities of these
+        linked sites. Nonetheless, we seek to protect the integrity of our site
+        and welcome any feedback about these sites.
+      </p>
+      </div>
+  );
+}
