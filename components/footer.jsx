99f1c540d8b8f2029d18e8ac51037f127b8ea51c
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-06 14:43:42 -0400

    feat: linqueando al papeleo de afuera

--- components/footer.jsx
+++ components/footer.jsx
@@ -6,13 +6,19 @@ export const Footer = () => (
       &copy; Creado por <a href="https://twitter.com/LimbatusDev">Limbatus</a>. Todos los derechos reservados
     </div>
     <div className="flex justify-around">
-      <Link href="/privacy">
-        <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+      <Link href="https://www.threadly.app/privacy">
+        <a
+          className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          target="_blank"
+        >
         Política de privacidad
         </a>
       </Link>
-      <Link href="/tac">
-        <a className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+      <Link href="https://www.threadly.app/terms">
+        <a
+          className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+          target="_blank"
+        >
         Términos y condiciones
         </a>
       </Link>
