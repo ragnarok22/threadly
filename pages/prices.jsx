febd211cd5d2cbd6ab05d9d44303d27056e98aaf
Reinier Hernández <sasuke.reinier@gmail.com>
2021-11-06 12:02:13 -0400

    fix: text in prices

--- pages/prices.jsx
+++ pages/prices.jsx
@@ -55,12 +55,12 @@ const Prices = () => {
       <div className="xl:mx-auto xl:container py-8 2xl:px-0 px-6">
         <div className="lg:flex items-center justify-between">
           <div className=" lg:w-1/2 w-full">
-            <p className="text-base leading-4 text-gray-600 dark:text-gray-300">Elige un plan que se adecue a ti</p>
+            <p className="text-base leading-4 text-gray-600 dark:text-gray-300">Elige un plan que se ajuste a ti</p>
             <h1 role="heading" className="md:text-5xl text-3xl font-bold leading-10 mt-3 text-gray-800 dark:text-gray-100">
               Elige tu plan
             </h1>
             <p role="contentinfo" className="text-base leading-5 mt-5 text-gray-600 dark:text-gray-300">
-            Comienza a gestionar tu contenido en Twitter de una forma fácil y sencilla. Trabajamos para que puedas gestionar mejor tu contenido en Twitter
+            Comienza a gestionar tu contenido en Twitter de una forma fácil y sencilla. Trabajamos para que puedas mejorar tu contenido en Twitter
             </p>
             <div className="w-56">
               <div className="bg-gray-100 dark:bg-gray-600 shadow rounded-full flex items-center mt-10">
@@ -78,7 +78,7 @@ const Prices = () => {
             <div role="listitem" className="bg-white dark:bg-gray-600 shadow rounded-lg p-8 relative z-30">
               <div className="md:flex items-center justify-between">
                 <h2 className="text-2xl font-semibold leading-6 text-gray-800 dark:text-gray-100">Starter</h2>
-                <p className="text-2xl font-semibold md:mt-0 mt-4 leading-6 text-gray-800 dark:text-gray-100">FREE</p>
+                <p className="text-2xl font-semibold md:mt-0 mt-4 leading-6 text-gray-800 dark:text-gray-100">GRATIS</p>
               </div>
               <p className="md:w-80 text-base leading-6 mt-4 text-gray-600 dark:text-gray-200">
                 Una forma sencilla de publicar tus hilos de forma organizada
