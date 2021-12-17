b30cb10dc2f2c21562e5cf9cbb21f4e5f0c7c569
Reinier Hernández <sasuke.reinier@gmail.com>
2021-12-17 16:40:12 -0500

    minimal changes

--- components/twitter/Thread.jsx
+++ components/twitter/Thread.jsx
@@ -1,8 +1,13 @@
+// import { PlusCircleIcon } from "@heroicons/react/outline";
 import { useSelector } from "react-redux";
 import { Tweet } from "./Tweet"
 
 export const Thread = ({ tweets, canTweet, setText }) => {
   const user = useSelector((state) => state.user);
+
+  // const addTeet = (e) => {
+  //   setText(tweets + "\n\n\nstart here...")
+  // }
 
   return (
     <div className="flex flex-col h-96 overflow-auto px-3">
@@ -13,6 +18,9 @@ export const Thread = ({ tweets, canTweet, setText }) => {
         })
         : <Tweet />
       }
+      {/* <div className="flex justify-center items-center mt-0">
+        <PlusCircleIcon className="h-6 w-6 cursor-pointer" aria-hidden="true" onClick={addTeet} />
+      </div> */}
     </div>
   )
 }
